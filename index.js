e6ab999dd5e262580fabb49bf1a3a20f5da6d026
Alexey-petr0v <59523324+Alexey-petr0v@users.noreply.github.com>
2020-09-15 14:03:17 +0700

    Внесены правки

--- index.js
+++ index.js
@@ -11,42 +11,38 @@ function moveRect(e){
     switch(e.keyCode){
         case 37:  // если нажата клавиша влево
             if ($(".elem[data-num='"+current_num+"'").data("left")) {
-                console.log("left")
-                console.log("current_num: "+current_num);
                 $(".check").removeClass("check");
                 current_num = +$(".elem[data-num='"+current_num+"'").data("left");
                 $(".elem[data-num='"+current_num+"'").addClass("check");
-            } else {console.log("current_num: "+current_num);console.log("влево запрещено")}
+                console.log("Click at rect "+$(".elem[data-num='"+current_num+"'").data("num"))
+            } else {console.log("влево запрещено")}
             break;
 
         case 38:   // если нажата клавиша вверх
         if ($(".elem[data-num='"+current_num+"'").data("top")) {
-            console.log("top")
-            console.log("current_num: "+current_num);
             $(".check").removeClass("check");
             current_num = +$(".elem[data-num='"+current_num+"'").data("top");
             $(".elem[data-num='"+current_num+"'").addClass("check");
-            } else {console.log("current_num: "+current_num);console.log("вверх запрещено")}
+            console.log("Click at rect "+$(".elem[data-num='"+current_num+"'").data("num"))
+            } else {console.log("вверх запрещено")}
             break;
 
         case 39:   // если нажата клавиша вправо
         if ($(".elem[data-num='"+current_num+"'").data("right")) {
-            console.log("right")
-            console.log("current_num: "+current_num);
             $(".check").removeClass("check");
             current_num = +$(".elem[data-num='"+current_num+"'").data("right");
             $(".elem[data-num='"+current_num+"'").addClass("check");
-            } else {console.log("current_num: "+current_num);console.log("вправо запрещено")}
+            console.log("Click at rect "+$(".elem[data-num='"+current_num+"'").data("num"))
+            } else {console.log("вправо запрещено")}
             break;
 
         case 40:   // если нажата клавиша вниз
         if ($(".elem[data-num='"+current_num+"'").data("bottom")) {
-            console.log("bottom")
-            console.log("current_num: "+current_num);
             $(".check").removeClass("check");
             current_num = +$(".elem[data-num='"+current_num+"'").data("bottom");
             $(".elem[data-num='"+current_num+"'").addClass("check");
-            } else {console.log("current_num: "+current_num);console.log("вниз запрещено")}
+            console.log("Click at rect "+$(".elem[data-num='"+current_num+"'").data("num"))
+            } else {console.log("вниз запрещено")}
             break;
     }
 }
